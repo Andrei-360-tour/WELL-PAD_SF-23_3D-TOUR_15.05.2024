41f0ae1c4eaceae78c67bb7291104bc405ace941
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-10 15:22:23 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,459 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-23_overview",
+      "name": "SF-23_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6001254332399153,
+        "pitch": 0.7157158588108352,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3163413888841227,
+          "pitch": 0.5653765631223902,
+          "rotation": 0,
+          "target": "1-sf-23_overview-1"
+        },
+        {
+          "yaw": -2.4494319134046663,
+          "pitch": 0.4664941824580726,
+          "rotation": 0,
+          "target": "2-sf-23_wellhead"
+        },
+        {
+          "yaw": 3.053025385288027,
+          "pitch": 0.9260627753500081,
+          "rotation": 0,
+          "target": "6-sf-23_old-security-building"
+        },
+        {
+          "yaw": -1.3721537906902377,
+          "pitch": 0.7346918683272889,
+          "rotation": 0,
+          "target": "5-sf-23_well-pad--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-23_overview-1",
+      "name": "SF-23_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.37160110313065076,
+        "pitch": 0.5810180992036251,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.962587198110066,
+          "pitch": -0.04569512624249583,
+          "rotation": 0,
+          "target": "0-sf-23_overview"
+        },
+        {
+          "yaw": -0.4391996604775734,
+          "pitch": 0.4525136692786944,
+          "rotation": 0,
+          "target": "2-sf-23_wellhead"
+        },
+        {
+          "yaw": -0.8174827064667749,
+          "pitch": 0.6443804072143937,
+          "rotation": 0,
+          "target": "3-sf-23_wellhead-1"
+        },
+        {
+          "yaw": -0.3054713244826388,
+          "pitch": 0.17142383220957846,
+          "rotation": 0,
+          "target": "7-sf-23_well-pad"
+        },
+        {
+          "yaw": -2.0730103642725872,
+          "pitch": 0.23302865465361577,
+          "rotation": 0,
+          "target": "6-sf-23_old-security-building"
+        },
+        {
+          "yaw": 2.739083730707504,
+          "pitch": 0.26110788607162405,
+          "rotation": 0,
+          "target": "5-sf-23_well-pad--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-23_wellhead",
+      "name": "SF-23_Wellhead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.032034127277025704,
+        "pitch": 0.28245933034227555,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8513097767777964,
+          "pitch": 0.26541981816791704,
+          "rotation": 0,
+          "target": "3-sf-23_wellhead-1"
+        },
+        {
+          "yaw": 0.2963154397572403,
+          "pitch": -0.3318731848096377,
+          "rotation": 0,
+          "target": "0-sf-23_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-23_wellhead-1",
+      "name": "SF-23_Wellhead 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.10368352727086894,
+        "pitch": 0.29983086497340494,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6380937876723589,
+          "pitch": 0.30559983955098247,
+          "rotation": 0,
+          "target": "2-sf-23_wellhead"
+        },
+        {
+          "yaw": 0.9979733567459146,
+          "pitch": 0.13708141467527746,
+          "rotation": 0,
+          "target": "4-sf-23_wellhead--pipeline"
+        },
+        {
+          "yaw": 0.7991011710599345,
+          "pitch": -0.30336027130658394,
+          "rotation": 0,
+          "target": "1-sf-23_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-23_wellhead--pipeline",
+      "name": "SF-23_Wellhead & Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.36384470567238836,
+        "pitch": 0.3465348526061227,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17308744558366485,
+          "pitch": 0.12496961560051645,
+          "rotation": 0,
+          "target": "3-sf-23_wellhead-1"
+        },
+        {
+          "yaw": -2.9103727313400007,
+          "pitch": 0.15829434308728274,
+          "rotation": 0,
+          "target": "5-sf-23_well-pad--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-23_well-pad--pipeline",
+      "name": "SF-23_Well Pad & pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.5706673011190864,
+        "pitch": 0.08797911185930651,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.760811948390698,
+          "pitch": 0.15568556676198853,
+          "rotation": 0,
+          "target": "4-sf-23_wellhead--pipeline"
+        },
+        {
+          "yaw": 1.771461881835621,
+          "pitch": 0.04092352759850115,
+          "rotation": 0,
+          "target": "6-sf-23_old-security-building"
+        },
+        {
+          "yaw": 2.8221949181456125,
+          "pitch": -0.17458355579363172,
+          "rotation": 0,
+          "target": "1-sf-23_overview-1"
+        },
+        {
+          "yaw": 1.0444065869539436,
+          "pitch": -0.2448814800519692,
+          "rotation": 0,
+          "target": "0-sf-23_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-23_old-security-building",
+      "name": "SF-23_Old security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.182647583295953,
+        "pitch": 0.15512110764698406,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.446454167817169,
+          "pitch": 0.12297542887898416,
+          "rotation": 0,
+          "target": "5-sf-23_well-pad--pipeline"
+        },
+        {
+          "yaw": 2.236538606173589,
+          "pitch": 0.024739367233209464,
+          "rotation": 0,
+          "target": "7-sf-23_well-pad"
+        },
+        {
+          "yaw": -0.8761141668783452,
+          "pitch": -0.2473072804992782,
+          "rotation": 0,
+          "target": "0-sf-23_overview"
+        },
+        {
+          "yaw": 2.7579304074848157,
+          "pitch": -0.03959239778654222,
+          "rotation": 0,
+          "target": "1-sf-23_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-23_well-pad",
+      "name": "SF-23_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.1495218997477252,
+        "pitch": 0.1290933157697456,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.063522157133793,
+          "pitch": 0.041308763796070735,
+          "rotation": 0,
+          "target": "6-sf-23_old-security-building"
+        },
+        {
+          "yaw": -2.8185557086938235,
+          "pitch": 0.1367736971088025,
+          "rotation": 0,
+          "target": "2-sf-23_wellhead"
+        },
+        {
+          "yaw": -2.725078390163862,
+          "pitch": -0.052347115464272065,
+          "rotation": 0,
+          "target": "1-sf-23_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-23",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
